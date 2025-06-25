23cdd924b7cf0d1f1db6af8cfd03e07a71207b75
engboris <engboris@hotmail.fr>
2025-06-25 23:55:56 +0200

    dune fmt

--- test/test.ml
+++ test/test.ml
@@ -23,7 +23,7 @@ let run_dir test_f directory =
 let () =
   Alcotest.run "Stellogen Test Suite"
     [ ("Stellogen examples", run_dir sgen "../examples/")
-    ;
-    (* ; ("Stellogen exercises solutions", run_dir sgen "../exercises/solutions/")
-    ; *) ("Stellogen syntax", run_dir sgen "./subjects/")
+    ; (* ; ("Stellogen exercises solutions", run_dir sgen "../exercises/solutions/")
+    ; *)
+      ("Stellogen syntax", run_dir sgen "./subjects/")
     ]
