116d63a59c49996af7b2401b7da8fe4275361cd9
engboris <engboris@hotmail.fr>
2025-10-30 20:21:49 +0100

    Add trace command for debug

--- web/playground.ml
+++ web/playground.ml
@@ -28,10 +28,34 @@ let run_stellogen code_js =
     Console.console##log (Js.string msg);
     Js.string msg
 
+(* Function that runs Stellogen code with trace and returns output *)
+let trace_stellogen code_js =
+  Console.console##log (Js.string "trace_stellogen called");
+  let code = Js.to_string code_js in
+  Console.console##log (Js.string ("Code: " ^ code));
+
+  try
+    let result = Web_interface.trace_from_string code in
+    Console.console##log (Js.string "trace_from_string returned");
+
+    match result with
+    | Ok output ->
+      Console.console##log (Js.string ("Success: " ^ output));
+      Js.string output (* Don't strip ANSI codes from HTML *)
+    | Error err ->
+      Console.console##log (Js.string ("Error: " ^ err));
+      Js.string ("ERROR: " ^ strip_ansi_codes err)
+  with e ->
+    let msg = "Exception in trace_stellogen: " ^ Printexc.to_string e in
+    Console.console##log (Js.string msg);
+    Js.string msg
+
 (* Export to JavaScript *)
 let () =
   Console.console##log (Js.string "Stellogen playground loaded");
   Js.export "Stellogen"
     object%js
       method run code = run_stellogen code
+
+      method trace code = trace_stellogen code
     end
