ab60dcaea494f437dfea648d913e64373bc3361c
Tau <git@alice-carroll.pet>
2026-06-26 00:32:47 +0300

    fix nix configuration

--- flake.nix
+++ flake.nix
@@ -2,65 +2,119 @@
   description = "stellogen";
 
   inputs = {
-    # Remark: when adding inputs here, don't forget to also add them in the
-    # arguments to `outputs` below!
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
+    };
   };
 
-  # Remark: keep the list of outputs in sync with the list of inputs above
-  # (see above remark)
-  outputs = { self, flake-utils, nixpkgs}:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        ocamlPackages = pkgs.ocamlPackages;
-        easy_logging = ocamlPackages.buildDunePackage rec {
-          pname = "easy_logging";
-          version = "0.8.2";
-          src = pkgs.fetchFromGitHub {
-            owner = "sapristi";
-            repo = "easy_logging";
-            rev = "v${version}";
-            sha256 = "sha256-Xy6Rfef7r2K8DTok7AYa/9m3ZEV07LlUeMQSRayLBco=";
+  outputs =
+    {
+      flake-parts,
+      systems,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      perSystem =
+        {
+          pkgs,
+          lib,
+          self',
+          ...
+        }:
+        {
+          packages = {
+            stellogen = pkgs.ocamlPackages.callPackage ./package.nix { };
+
+            stellogen-web = (self'.packages.stellogen.override { withExe = false; }).js;
+            stellogen-minimal = self'.packages.stellogen.override {
+              withWeb = false;
+              withDoc = false;
+            };
+
+            docs =
+              (self'.packages.stellogen.override {
+                withExe = false;
+                withWeb = false;
+              }).doc;
+
+            playground = pkgs.stdenvNoCC.mkDerivation {
+              pname = "stellogen-playground";
+              version = "0.1.0";
+              src =
+                let
+                  inherit (lib) fileset;
+                in
+                fileset.toSource {
+                  root = ./.;
+                  fileset = fileset.unions [
+                    ./examples
+                    ./web/build-examples.js
+                    ./web/index.html
+                  ];
+                };
+              buildPhase = ''
+                runHook preBuild
+                node ./web/build-examples.js
+                runHook postBuild
+              '';
+              nativeBuildInputs = [
+                pkgs.nodejs-slim
+              ];
+              installPhase = ''
+                runHook preInstall
+                mkdir -p "$out/share/web/"
+                cp web/index.html web/examples.js  "$out/share/web/"
+                cp ${self'.packages.stellogen-web}/share/web/playground.js "$out/share/web/"
+                runHook postInstall
+              '';
+            };
+
+            default = self'.packages.stellogen;
           };
-          buildInputs = [ ocamlPackages.calendar ];
-        };
 
-        stellogen = ocamlPackages.buildDunePackage {
-          pname = "stellogen";
-          version = "0.1.0";
-          duneVersion = "3";
-          src = ./.;
-          OCAMLPARAM = "_,warn-error=+A"; # Turn all warnings into errors.
-          propagatedBuildInputs = [
-            easy_logging
-          ] ++ (with ocamlPackages; [
-	    calendar
-	    alcotest
-	    base
-	    menhir
-          ]);
-        };
-      in
-      {
-        packages = {
-          inherit stellogen;
-          default = stellogen;
-        };
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.ocamlPackages.ocaml
-            pkgs.ocamlPackages.ocamlformat
-            pkgs.ocamlPackages.menhir
-            pkgs.ocamlPackages.odoc
-	    pkgs.ocamlPackages.ocaml-lsp
-            pkgs.jq
-          ];
+          apps = {
+            playground.program = pkgs.writeShellApplication {
+              name = "stellogen-playground-server";
+              runtimeInputs = [ pkgs.simple-http-server ];
+              text = ''
+                cd ${lib.escapeShellArg self'.packages.playground}/share/web
+                simple-http-server --index "$@" -- .
+              '';
+            };
+            docs.program = pkgs.writeShellApplication {
+              name = "stellogen-docs-server";
+              runtimeInputs = [ pkgs.simple-http-server ];
+              text = ''
+                cd ${lib.escapeShellArg self'.packages.docs}/share/doc/stellogen/_html
+                simple-http-server --index "$@" -- .
+              '';
+            };
+          };
 
-          inputsFrom = [
-            self.packages.${system}.stellogen
-          ];
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.jq
+            ]
+            ++ (with pkgs.ocamlPackages; [
+              ocaml
+              ocamlformat
+              menhir
+              odoc
+              ocaml-lsp
+            ]);
+
+            inputsFrom = [
+              self'.packages.stellogen
+            ];
+          };
         };
-      });
+    };
 }
